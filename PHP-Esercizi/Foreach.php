a8ba4dbee3bef7237d13476b101393fcadf04c10
Draxer98 <150442304+Draxer98@users.noreply.github.com>
2026-05-17 15:25:31 +0200

    Delete PHP-Esercizi directory

--- PHP-Esercizi/Foreach.php
+++
@@ -1,7 +0,0 @@
-<?php
-$somma=0;
-$ris=0;
-
-foreach ($_POST as $key => $value) {
-    echo "Il campo POST: [".$key."] contiene il valore = ".$value."<br>";
-}
--- PHP-Esercizi/Scacchiera.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "Scacchiera";
-include("firstphpevercreated.php");
-disegnaTabella(100,100);
